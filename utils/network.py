73da3bbd584df2bcde2f523f59c4b910d42eaef1
Jongwan Kim <figurekim317@gmail.com>
2023-04-04 21:39:38 +0900

    Update network.py

--- utils/network.py
+++ utils/network.py
@@ -1,7 +1,4 @@
 
-
-def init_weights(net, init_type='normal', gain=0.02):
-    def init_func(m):
         classname = m.__class__.__name__
         if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
             if init_type == 'normal':
